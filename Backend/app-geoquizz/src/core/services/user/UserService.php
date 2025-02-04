7ab64a14a3367935d70a0a4e6e5b1ea5f8694dee
Math.dlcrt <mathias.delcourt4@etu.univ-lorraine.fr>
2025-02-04 16:19:04 +0100

    ajout du service user

---
+++ Backend/app-geoquizz/src/core/services/user/UserService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace api_geoquizz\core\services\user;
+
+use api_geoquizz\core\dto\UserDTO;
+use api_geoquizz\core\services\user\UserServiceInterface;
+use api_geoquizz\core\repositoryInterface\user\UserRepositoryInterface;
+
+class UserService implements UserServiceInterface {
+
+    protected UserRepositoryInterface $userRepository;
+
+    public function __construct(UserRepositoryInterface $userRepository) {
+        $this->userRepository = $userRepository;
+    }
+
+    public function createUser(UserDTO $user): void {
+        $this->userRepository->createUser($user->ID, $user->email, $user->nickname);
+    }
+
+    public function getUserById(string $id): UserDTO {
+        $user = $this->userRepository->getUserById($id);
+        return new UserDTO($user->getId(), $user->getNickName(), $user->getEmail());
+    }
+    public function getUserByEmail(string $email): UserDTO {
+        $user = $this->userRepository->getUserByEmail($email);
+        return new UserDTO($user->getId(), $user->getNickName(), $user->getEmail());
+    }
+}
