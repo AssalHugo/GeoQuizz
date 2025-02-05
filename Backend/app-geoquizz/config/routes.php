64893ec0ca9c15d222209f91d41106d005623a05
ismael kone <ismael.kone4@etu.univ-lorraine.fr>
2025-02-05 17:20:47 +0100

    Réparation des routes

--- Backend/app-geoquizz/config/routes.php
+++ Backend/app-geoquizz/config/routes.php
@@ -1,11 +1,12 @@
 <?php
 declare(strict_types=1);
 
-use api_geoquizz\application\actions\CreateGameAction;
-use api_geoquizz\application\actions\EndGameAction;
+
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
-use api_geoquizz\application\actions\GameAction;
+use Slim\App;
+use api_geoquizz\application\actions\CreateGameAction;
+use api_geoquizz\application\actions\EndGameAction;
 use api_geoquizz\application\actions\GetCurrentPhotoAction;
 use api_geoquizz\application\actions\GetGameAction;
 use api_geoquizz\application\actions\GetGameScoreAction;
@@ -13,40 +14,28 @@ use api_geoquizz\application\actions\GetGameStateAction;
 use api_geoquizz\application\actions\HomeAction;
 use api_geoquizz\application\actions\PlayGameAction;
 use api_geoquizz\application\actions\StartGameAction;
+use api_geoquizz\application\actions\CreateUserAction;
+use api_geoquizz\application\actions\GetUserByEmailAction;
+use api_geoquizz\application\actions\GetUserByIdAction;
 
-return function (\Slim\App $app): \Slim\App {
-    // Route d'accueil
+return function (App $app): App {
+    // Page d'accueil
     $app->get('/', HomeAction::class);
 
-    // Routes pour le jeu
-    $app->group('/games', function($app) {
-        // Créer une nouvelle partie
-        $app->post('', [CreateGameAction::class, 'createGame']);
-        
-        // Récupérer une partie spécifique
-        $app->get('/{id}', [GetGameAction::class, 'getGame']);
-        
-        // Démarrer une partie
-        $app->patch('/{id}/start', [StartGameAction::class, 'startGame']);
-        
-        // Soumettre une réponse pour la photo courante
-        $app->post('/{id}/answer', [PlayGameAction::class, 'submitAnswer']);
-        
-        // Terminer une partie
-        $app->patch('/{id}/end', [EndGameAction::class, 'endGame']);
-        
-        // Obtenir la photo courante d'une partie
-        $app->get('/{id}/current-photo', [GetCurrentPhotoAction::class, 'getCurrentPhoto']);
-        
-        // Obtenir le score actuel d'une partie
-        $app->get('/{id}/score', [GetGameScoreAction::class, 'getScore']);
-        
-        // Obtenir l etat d'une partie
-        $app->get('/{id}/state', [GetGameStateAction::class, 'getStatus']);
-    });
-    $app->post('/users', \api_geoquizz\application\actions\CreateUserAction::class);
-    $app->get('/users/{id}', \api_geoquizz\application\actions\GetUserByIdAction::class);
-    $app->get('/users', \api_geoquizz\application\actions\GetUserByEmailAction::class);
+    // Routes pour les jeux
+    $app->post('/games', CreateGameAction::class);
+    $app->get('/games/{id}', GetGameAction::class);
+    $app->patch('/games/{id}/start', StartGameAction::class);
+    $app->post('/games/{id}/answer', PlayGameAction::class);
+    $app->patch('/games/{id}/end', EndGameAction::class);
+    $app->get('/games/{id}/current-photo', GetCurrentPhotoAction::class);
+    $app->get('/games/{id}/score', GetGameScoreAction::class);
+    $app->get('/games/{id}/state', GetGameStateAction::class);
+
+    // Routes pour les utilisateurs
+    $app->post('/users', CreateUserAction::class);
+    $app->get('/users/{id}', GetUserByIdAction::class);
+    $app->get('/users', GetUserByEmailAction::class);
 
     return $app;
 };
