a4bc02f8fd8745b7da2916d083cd34ea334cfb26
Math.dlcrt <mathias.delcourt4@etu.univ-lorraine.fr>
2025-02-06 15:42:36 +0100

    Création table

--- Backend/sql/GEOQUIZZ_servicesGeoquizz.sql
+++ Backend/sql/GEOQUIZZ_servicesGeoquizz.sql
@@ -1,12 +1,12 @@
-CREATE TABLE game (
-    id VARCHAR(48) PRIMARY KEY,
-    userId UUID NOT NULL,
-    photoIds JSONB NOT NULL,
-    serieId VARCHAR(48) NOT NULL,
-    score INT DEFAULT 0 NOT NULL,
-    state VARCHAR(20) NOT NULL,
-    currentPhotoIndex INT DEFAULT 0 NOT NULL,
-    startTime TIMESTAMP DEFAULT NULL
+CREATE TABLE "game" (
+    "id" VARCHAR(48) PRIMARY KEY,
+    "userId" UUID NOT NULL,
+    "photoIds" JSONB NOT NULL,
+    "serieId" VARCHAR(48) NOT NULL,
+    "score" INT DEFAULT 0 NOT NULL,
+    "state" VARCHAR(20) NOT NULL,
+    "currentPhotoIndex" INT DEFAULT 0 NOT NULL,
+    "startTime" TIMESTAMP DEFAULT NULL
 );
 
 CREATE TABLE "user" (
