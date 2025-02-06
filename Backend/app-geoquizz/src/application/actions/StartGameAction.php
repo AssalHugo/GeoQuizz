735fa3d638802df8b05056f3606b4debda04af20
Math.dlcrt <mathias.delcourt4@etu.univ-lorraine.fr>
2025-02-06 10:31:30 +0100

    Ajout des try & catch de StartGameAction

--- Backend/app-geoquizz/src/application/actions/StartGameAction.php
+++ Backend/app-geoquizz/src/application/actions/StartGameAction.php
@@ -4,19 +4,25 @@ namespace api_geoquizz\application\actions;
 
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
-use api_geoquizz\core\services\GameService;
+use api_geoquizz\core\services\GameServiceInterface;
 use api_geoquizz\application\renderer\JsonRenderer;
-use api_geoquizz\core\services\game\GameNotFoundException;
-use api_geoquizz\core\services\game\GameServiceException;
+use api_geoquizz\core\repositoryInterface\RepositoryEntityNotFoundException;
+use api_geoquizz\core\repositoryInterface\RepositoryEntityConflictException;
+use api_geoquizz\core\repositoryInterface\RepositoryEntityValidationException;
+use api_geoquizz\core\repositoryInterface\RepositoryConnectionException;
+use api_geoquizz\core\repositoryInterface\RepositoryException;
 
-class StartGameAction extends AbstractAction {
-    private GameService $gameService;
-    
-    public function __construct(GameService $gameService) {
+class StartGameAction extends AbstractAction
+{
+    private GameServiceInterface $gameService;
+
+    public function __construct(GameServiceInterface $gameService)
+    {
         $this->gameService = $gameService;
     }
-    
-    public function __invoke(Request $rq, Response $rs, array $args): Response {
+
+    public function __invoke(Request $rq, Response $rs, array $args): Response
+    {
         if (!isset($args['id'])) {
             $data = [
                 'message' => 'Missing required parameter: gameId',
@@ -33,20 +39,10 @@ class StartGameAction extends AbstractAction {
         try {
             $game = $this->gameService->getGameById($args['id']);
             $this->gameService->startGame($game);
-
+        } catch (RepositoryEntityNotFoundException $e) {
             $data = [
-                'status' => 'Game started',
-                'links' => [
-                    'self' => ['href' => '/games/' . $args['id']]
-                ]
-            ];
-
-            return JsonRenderer::render($rs, 200, $data);
-
-        } catch (\Exception $e) {
-            $data = [
-                'message' => 'Game not found',
-                'exception' => [
+                'message' => $e->getMessage(),
+                'error' => [
                     'type' => get_class($e),
                     'code' => 404,
                     'file' => $e->getFile(),
@@ -54,23 +50,54 @@ class StartGameAction extends AbstractAction {
                 ]
             ];
             return JsonRenderer::render($rs, 404, $data);
-
-        } catch (\Exception $e) {
+        } catch (RepositoryEntityConflictException $e) {
             $data = [
-                'message' => 'Error starting the game',
-                'exception' => [
+                'message' => $e->getMessage(),
+                'error' => [
                     'type' => get_class($e),
-                    'code' => $e->getCode(),
+                    'code' => 409,
+                    'file' => $e->getFile(),
+                    'line' => $e->getLine()
+                ]
+            ];
+            return JsonRenderer::render($rs, 409, $data);
+        } catch (RepositoryEntityValidationException $e) {
+            $data = [
+                'message' => $e->getMessage(),
+                'error' => [
+                    'type' => get_class($e),
+                    'code' => 400,
+                    'file' => $e->getFile(),
+                    'line' => $e->getLine()
+                ]
+            ];
+            return JsonRenderer::render($rs, 400, $data);
+        } catch (RepositoryConnectionException $e) {
+            $data = [
+                'message' => $e->getMessage(),
+                'error' => [
+                    'type' => get_class($e),
+                    'code' => 503,
+                    'file' => $e->getFile(),
+                    'line' => $e->getLine()
+                ]
+            ];
+            return JsonRenderer::render($rs, 503, $data);
+        } catch (RepositoryException $e) {
+            $data = [
+                'message' => $e->getMessage(),
+                'error' => [
+                    'type' => get_class($e),
+                    'code' => 500,
                     'file' => $e->getFile(),
                     'line' => $e->getLine()
                 ]
             ];
             return JsonRenderer::render($rs, 500, $data);
-
         } catch (\Exception $e) {
             $data = [
-                'message' => 'An unexpected error occurred.',
-                'exception' => [
+                'message' => $e->getMessage(),
+                'error' => [
                     'type' => get_class($e),
                     'code' => 500,
                     'file' => $e->getFile(),
@@ -79,5 +106,14 @@ class StartGameAction extends AbstractAction {
             ];
             return JsonRenderer::render($rs, 500, $data);
         }
+
+        $data = [
+            'status' => 'Game started',
+            'links' => [
+                'self' => ['href' => '/games/' . $args['id']]
+            ]
+        ];
+
+        return JsonRenderer::render($rs, 200, $data);
     }
 }
